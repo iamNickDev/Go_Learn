51c279c38b7b1784c86d451d6934aef477d8a3d6
iamNickDev <nandkishoregeete@gmail.com>
2023-12-07 11:56:58 +0530

    learn loop if/else

--- Go_Learn.go
+++ Go_Learn.go
@@ -53,4 +53,64 @@ func main() {
 	fmt.Println(int64(dec))
 
 	fmt.Println(math.Sin(n))
+
+	// For Loop
+	//The most basic type, with a single condition.
+
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
+
+	//A classic initial/condition/after for loop.
+
+	for j := 7; j <= 9; j++ {
+		fmt.Println(j)
+	}
+	//for without a condition will loop repeatedly until you -
+	// break out of the loop or return from the enclosing function.
+
+	for {
+		fmt.Println("loop")
+		break
+	}
+
+	//You can also continue to the next iteration of the loop.
+	for n := 0; n <= 5; n++ {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+
+	// If/Else
+	//Here’s a basic example.
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+
+	//You can have an if statement without an else.
+	if 8%2 == 0 {
+		fmt.Println("8 is divisible by 2")
+	}
+
+	//Logical operators like && and || are often useful in conditions.
+
+	if 7%2 == 0 || 8%2 == 0 {
+		fmt.Println("either 8 or 7 are even")
+	}
+
+	//A statement can precede conditionals; any variables declared in this
+	// statement are available in the current and all subsequent branches.
+
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negetive")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
 }
