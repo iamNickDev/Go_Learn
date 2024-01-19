97edf3ec20150f775800d89e588709c2bea215c2
iamNickDev <nandkishoregeete@gmail.com>
2024-01-19 13:12:29 +0530

    Strings and Runes

--- Functions/function.go
+++ Functions/function.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"unicode/utf8"
 )
 
 func plus(a int, b int) int {
@@ -93,12 +94,42 @@ func main() {
 	i := 1
 	fmt.Println("initial:", i)
 
-	zeroval(&i)
+	zeroval(i)
 	fmt.Println("zeroval:", i)
 
-	zeroptr(i)
+	zeroptr(&i)
 	fmt.Println("zeroptr:", i)
 
 	fmt.Println("pointer:", &i)
 
+	const s = "राधा"
+
+	fmt.Println("len:", len(s))
+
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%x ", s[i])
+	}
+
+	fmt.Println()
+
+	fmt.Println("Rune count: ", utf8.RuneCountInString(s))
+
+	for idx, runeValue := range s {
+		fmt.Printf("%#U starts at %d\n", runeValue, idx)
+	}
+	fmt.Println("\nUsing DecodeRuneInString")
+	for i, w := 0, 0; i < len(s); i += w {
+		runeValue, width := utf8.DecodeRuneInString(s[i:])
+		fmt.Printf("%#U starts at %d\n", runeValue, i)
+		w = width
+		examineRune(runeValue)
+	}
+}
+
+func examineRune(r rune) {
+	if r == 't' {
+		fmt.Println("found tee")
+	} else if r == 'र' {
+		fmt.Println("found devnagri letter RA")
+	}
 }
