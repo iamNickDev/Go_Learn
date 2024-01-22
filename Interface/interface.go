ccd4148c789004fc0fee75506de107760783b865
iamNickDev <nandkishoregeete@gmail.com>
2024-01-22 09:57:48 +0530

    Learn Interface by multilpe struct in Go

--- Interface/interface.go
+++ Interface/interface.go
@@ -10,16 +10,30 @@ type Rectangle struct {
 	length, breadth float32
 }
 
+func (t Tringle) area() float32 {
+	return 0.5 * t.base * t.height
+}
+
 func (r Rectangle) area() float32 {
 	return r.length * r.breadth
 }
 
-func calculate(s Shape) {
-	fmt.Println("Area:", s.area())
+type Tringle struct {
+	base, height float32
+}
+
+func calculate(s Shape) float32 {
+	return s.area()
 }
 
 func main() {
-	rect := Rectangle{7, 4}
 
-	calculate(rect)
+	r := Rectangle{7, 4}
+	t := Tringle{8, 12}
+
+	rectangleArea := calculate(r)
+	fmt.Println("Area of Rectangle:", rectangleArea)
+
+	triangleArea := calculate(t)
+	fmt.Println("Area of Triangle:", triangleArea)
 }
