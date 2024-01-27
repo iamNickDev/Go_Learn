396e7ff1f7c3d1b542a197575d535b28f270313e
iamNickDev <nandkishoregeete@gmail.com>
2024-01-27 13:46:45 +0530

    Learned take_input in Go

--- Take_Input/take_input.go
+++ Take_Input/take_input.go
@@ -6,21 +6,40 @@
 
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 
 	// create variables
 	var name string
 	var age int
+	var temperature float32
+	var sunny bool
 
 	// take name and age input
-	fmt.Println("Enter your name and age: \nNandkishore 24")
+	fmt.Println("Enter your name and age: Nandkishore", 24)
 	fmt.Scan(&name, &age)
 
 	// print input values
 	fmt.Printf("Name: %s\nAge: %d", name, age)
 
+	fmt.Println("Enter your name and age: Nandkishore", 24)
+	fmt.Scanln(&name, &age)
+	fmt.Printf("Name: %s\nAge: %d", name, age)
+
+	// take name and age input using format specifier
+	fmt.Println("Enter your name and age: Nnadkishore ", 25)
+	fmt.Scanf("%s %d", &name, &age)
+
+	fmt.Printf("Name: %s\nAge: %d", name, age)
+	// Take user input for temperature
+	fmt.Println("Enter the current temperature:")
+	fmt.Scanf("%f", &temperature)
+
+	// Take user input for whether the day is sunny
+	fmt.Println("Is the day sunny? (true/false):")
+	fmt.Scanf("%t", &sunny)
+
+	// Display the input values
+	fmt.Printf("Current temperature: %g\nIs the day Sunny? %t", temperature, sunny)
 }
