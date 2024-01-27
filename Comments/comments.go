583e6f88eace99fc870aee0b5def66d01aa68023
iamNickDev <nandkishoregeete@gmail.com>
2024-01-27 13:56:52 +0530

    Learned Comments in Go

---
+++ Comments/comments.go
@@ -0,0 +1,25 @@
+// In Go, there are two types of comments:
+
+//  - Single Line comments
+// /*...*/ - Multiline Comments
+
+package main
+
+import "fmt"
+
+func main() {
+
+	// declare a variable
+	age := 25
+	// height := 170
+
+	/* creates a variable
+	to store the salary of the employee
+	*/
+	salary := 30000
+
+	// print the variable
+	fmt.Println("Age is", age)
+	fmt.Println("Salary:", salary)
+
+}
