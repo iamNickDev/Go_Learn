1a465d11096c57706ca98322c29a4a950221e678
iamNickDev <nandkishoregeete@gmail.com>
2024-02-04 17:44:25 +0530

    Variable Scope in go

---
+++ Variable_Scope/variables_scope.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// outside the function
+var sum int
+
+// Go Local Variables
+func addNumbers() {
+	// var sum int
+	sum = 10 + 2
+}
+
+func main() {
+	addNumbers()
+
+	// cannot access sum out of its local scope
+	// fmt.Println("Sum is: ", sum)
+
+	fmt.Println("Plus is", sum)
+}
