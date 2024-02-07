392e7ef5dfc7afd340fe6199676e7d3d8731a246
iamNickDev <nandkishoregeete@gmail.com>
2024-02-07 23:39:15 +0530

    Learn Go

--- Packages/packages.go
+++ Packages/packages.go
@@ -2,7 +2,7 @@
 package main
 
 import (
-	"Packages/Calculator/calculator.go"
+	// "Packages/Calculator/calculator.go"
 	"fmt"
 	"math"
 	"strings"
@@ -63,13 +63,13 @@ func main() {
 	joinedString := strings.Join(stringArray, " ")
 	fmt.Println(joinedString)
 
-	number1 := 9
-	number2 := 5
+	// number1 := 9
+	// number2 := 5
 
 	// use the add function of calculator package
-	fmt.Println(calculator.Add(number1, number2))
+	// fmt.Println(calculator.Add(number1, number2))
 
 	// use the subtract function of calculator package
-	fmt.Println(calculator.Subtract(number1, number2))
+	// fmt.Println(calculator.Subtract(number1, number2))
 
 }
